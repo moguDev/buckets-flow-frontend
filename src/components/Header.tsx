e56f2a4d59916973f2aed89e2272015647b141ea
moguDev <mail@sho-horiguchi.jp>
2024-08-04 23:24:05 +0900

    2024.8.4

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="fixed top-0 h-16 w-full px-5 z-50">
       <div className="flex items-center h-full">
-        <p className="text-blue-500 text-2xl font-bold brightness-200">RAINY</p>
+        <p className="text-blue-300 text-2xl font-bold brightness-120">RAINY</p>
       </div>
     </header>
   );
