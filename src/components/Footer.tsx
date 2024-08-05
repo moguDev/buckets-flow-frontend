e36842a2560d25518b1f5ffa8c6475d74446421e
moguDev <mail@sho-horiguchi.jp>
2024-08-05 13:54:00 +0900

    2024.8.5

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 h-16">
-      <p>
-        Special-Thanks to All members of <a>RUNTEQ.</a>
+    <footer className="fixed bottom-0 w-full p-1 opacity-50">
+      <p className="text-sm font-thin text-center text-blue-200">
+        Special thanks to all members of{" "}
+        <a href="https://runteq.jp/" target="_blank" className="font-normal">
+          RUNTEQ.
+        </a>
       </p>
     </footer>
   );
