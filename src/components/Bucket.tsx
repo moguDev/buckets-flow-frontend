6afe935049fd5c4fcc3d7bf1d51f8e15d7ba1087
moguDev <mail@sho-horiguchi.jp>
2024-08-05 12:20:46 +0900

    2024.8.5

--- src/components/Bucket.tsx
+++ src/components/Bucket.tsx
@@ -8,10 +8,19 @@ const Bucket: React.FC<BucketProps> = ({ filled }) => {
   const waterHeight = `${filled}%`;
 
   return (
-    <div className="flex justify-center items-end h-80">
-      <div className="relative w-48 h-64 border-l-8 border-r-8 border-b-8 border-t-8 border-blue-300 rounded-b-2xl overflow-hidden">
+    <div className={`flex justify-center items-end h-80 opacity-90`}>
+      <div
+        className={`relative w-12 h-14 border-r-4 border-l-4 border-b-4 ${
+          filled === 100 ? "border-blue-400" : "border-gray-300"
+        } rounded-b-lg`}
+      >
         <div
-          className="absolute bottom-0 w-full bg-blue-300"
+          className={`absolute top-0 right-0 h-1 w-14 z-10 ${
+            filled === 100 ? "bg-blue-400" : "bg-gray-300"
+          }`}
+        />
+        <div
+          className="absolute bottom-0 w-full bg-blue-400"
           style={{ height: waterHeight }}
         ></div>
       </div>
