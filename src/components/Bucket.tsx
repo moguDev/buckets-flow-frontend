99d080786819fb5658c4d514dfee45ffbfd76872
moguDev <mail@sho-horiguchi.jp>
2024-08-05 13:22:55 +0900

    2024.8.5

--- src/components/Bucket.tsx
+++ src/components/Bucket.tsx
@@ -11,14 +11,16 @@ const Bucket: React.FC<BucketProps> = ({ filled, active = false }) => {
   return (
     <div className={`flex justify-center items-center opacity-60 px-3`}>
       <div
-        className={`relative w-6 h-7 border-r-2 border-l-2 border-b-2 ${
+        className={`relative ${
+          active ? "w-7 h-8" : "w-6 h-7"
+        } border-r-2 border-l-2 border-b-2 ${
           filled === 100 ? "border-blue-300" : "border-gray-100"
         } rounded-b`}
       >
         <div
-          className={`absolute top-0 right-0 h-0.5 w-7 z-10 ${
-            filled === 100 ? "bg-blue-300" : "bg-gray-100"
-          }`}
+          className={`absolute top-0 right-0 h-0.5 ${
+            active ? "w-8" : "w-7"
+          } z-10 ${filled === 100 ? "bg-blue-300" : "bg-gray-100"}`}
         />
         <div
           className="absolute bottom-0 w-full bg-blue-300 z-[-1]"
