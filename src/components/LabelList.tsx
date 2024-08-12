c9f427dcc9697f0329341c4a1781181fc1b75ebd
moguDev <mail@sho-horiguchi.jp>
2024-08-12 19:10:46 +0900

    fix label

--- src/components/LabelList.tsx
+++ src/components/LabelList.tsx
@@ -13,9 +13,10 @@ export const LabelList = () => {
     <div className="relative items-center w-full bg-gray-700 bg-opacity-10 p-1 mb-3 rounded-xl">
       <div className="grid lg:grid-cols-6 grid-cols-3 bg-opacity-0">
         {tags.map(
-          (tag) =>
+          (tag, index) =>
             tag !== selectedTag && (
               <button
+                key={index}
                 onClick={() => setSelectedTag(tag)}
                 className={`text-blue-300 bg-blue-800 bg-opacity-10 text-sm rounded-lg p-2 m-1 cursor-pointer select-none`}
               >
